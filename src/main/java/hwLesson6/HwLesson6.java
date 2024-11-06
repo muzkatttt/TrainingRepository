2517fee26c56f10b17e9256125831fc458c308d1
Katerina <emrodionova@bk.ru>
2024-11-06 10:38:41 +0300

    finished hw lesson6 StreamApi

--- src/main/java/hwLesson6/HwLesson6.java
+++ src/main/java/hwLesson6/HwLesson6.java
@@ -20,23 +20,82 @@ public class HwLesson6 {
                 new Transaction(mario, 2012, 700),
                 new Transaction(alan, 2012, 950)
         );
-        task1(transactions);
+//        task1(transactions);
+//        task2(transactions);
+//        task3(transactions);
+//        task4(transactions);
+//        task5(transactions);
+//        task6(transactions);
+        task7(transactions);
+        task8(transactions);
     }
 
-    //Найти все транзакции за 2011 год и отсортировать их по сумме (от меньшей к большей).
+    //1. Найти все транзакции за 2011 год и отсортировать их по сумме (от меньшей к большей).
     public static void task1(List<Transaction> transactions) {
         transactions.stream()
-                .filter((x) -> x.getYear() <2012)
+                .filter((x) -> x.getYear() < 2012)
                 .sorted(Comparator.comparingInt(Transaction::getValue))
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
 //        for (Transaction t : transactions){
 //            System.out.println(t);
-//        }
-
     }
+
+    //2. Вывести список неповторяющихся городов, в которых работают трейдеры.
+    public static void task2(List<Transaction> transactions) {
+        transactions.stream()
+                .map(x -> x.getTrader().getCity())
+                .distinct()
+                .forEach(System.out::println);
+    }
+
+    //3. Найти всех трейдеров из Кембриджа и отсортировать их по именам.
+    public static void task3(List<Transaction> transactions){
+        transactions.stream()
+                .filter(trader-> trader.getTrader().getCity().equals("Cambridge"))
+                .sorted((trader1, trader2) -> trader1.getTrader().getName().compareTo(trader2.getTrader().getName()))
+                .forEach(System.out::println);
+    }
+
+    //4. Вернуть строку со всеми именами трейдеров, отсортированными в алфавитном порядке
+    public static void task4(List<Transaction>transactions){
+        transactions.stream()
+                .map(trader -> trader.getTrader().getName())
+                .sorted((trader1, trader2) -> trader1.compareTo(trader2))
+                .distinct()
+                .forEach(System.out::println);
+    }
+
+    // 5. Выяснить, существует ли хоть один трейдер из Милана
+    public static void task5(List<Transaction>transactions) {
+        transactions.stream()
+                .filter(trader -> trader.getTrader().getCity().equals("Milan"))
+                .findFirst()
+                .ifPresent(System.out::println);
+    }
+
+    // 6. Вывести суммы всех транзакций трейдеров из Кембриджа
+    public static void task6(List<Transaction>transactions){
+        transactions.stream()
+                .map(Transaction::getValue)
+                .reduce(Integer::sum)
+                .ifPresent(System.out::println);
+    }
+
+    //7. Какова максимальная сумма среди всех транзакций?
+    public static void task7(List<Transaction>transactions){
+        transactions.stream()
+                .map(Transaction::getValue)
+                .reduce(Integer::max)
+                .ifPresent(System.out::println);
+    }
+
+    // 8. Найти транзакцию с минимальной суммой
+    public static void task8(List<Transaction>transactions){
+        transactions.stream()
+                .map(Transaction::getValue)
+                .reduce(Integer::min)
+                .ifPresent(System.out::println);
+    }
+
 }
-//sorted(Comparator.comparingInt(Transaction::getAmount))
-//                .collect(Collectors.toList());
-//
-//        transactions2011.forEach(System.out::println);
