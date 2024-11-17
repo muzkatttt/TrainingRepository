8fb47432becbc2b30398a9d2ba6050493d001eaa
Katerina <emrodionova@bk.ru>
2024-11-17 18:24:39 +0300

    regex fix + html + xml + json

---
+++ src/main/java/patterns/tasks/Task1.java
@@ -0,0 +1,19 @@
+package patterns.tasks;
+
+import java.util.regex.Pattern;
+
+/*
+1. Написать программу, проверяющую, является ли введённая
+строка адресом почтового ящика.
+В названии почтового ящика разрешить использование только
+букв, цифр и нижних подчёркиваний, при этом оно должно
+начинаться с буквы.
+Возможные домены верхнего уровня: .org .com
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        String regex = "[a-zA-Z]\\w*@\\w{3,}\\.(ru|com)";
+        String input = "ptushagds@gmail.com";
+        System.out.println(Pattern.matches(regex, input));
+    }
+}
